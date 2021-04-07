25cab580dad17febfce5b1123987f5b79f8c72b7
Vlad Pavlenko <pavlenko0902@gmail.com>
2021-04-07 16:16:28 +0300

    Initial Commit

---
+++ multiverse-leader-election-challenge/multiverse_leader_election_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiverse_leader_election_challengeApp.swift
+//  multiverse-leader-election-challenge
+//
+//  Created by Vladislav Pavlenko on 07.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct multiverse_leader_election_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
